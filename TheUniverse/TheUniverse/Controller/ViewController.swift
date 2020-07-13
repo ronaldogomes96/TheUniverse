7cf5e4a01e578eb0310a4cea41b26c222646073e
ronaldogomes96 <gomes.ronaldo96@gmail.com>
2020-07-13 15:04:59 -0300

    Added the initial project with folders according to the MVC model

---
+++ TheUniverse/TheUniverse/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TheUniverse
+//
+//  Created by Ronaldo Gomes on 13/07/20.
+//  Copyright © 2020 Ronaldo Gomes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
