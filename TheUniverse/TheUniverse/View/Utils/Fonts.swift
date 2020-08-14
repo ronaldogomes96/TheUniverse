8e2ea2c92ad9c7624f6b5d17cf29e41e99c72a30
ronaldogomes96 <gomes.ronaldo96@gmail.com>
2020-08-14 10:51:34 -0300

    Creating the extension with font types and sizes

---
+++ TheUniverse/TheUniverse/View/Utils/Fonts.swift
@@ -0,0 +1,14 @@
+//
+//  Fonts.swift
+//  TheUniverse
+//
+//  Created by Ronaldo Gomes on 14/08/20.
+//  Copyright © 2020 Ronaldo Gomes. All rights reserved.
+//
+
+import UIKit
+
+extension UIFont {
+    static let SFProRoundedTitle = UIFont(name: "SF Pro Rounded", size: 21)
+    static let SFProRoundedDescription = UIFont(name: "SF Pro Rounded", size: 19)
+}
