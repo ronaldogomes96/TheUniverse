9ec44d9daa88cb08ae8820bf5da6a19cd3eab0b6
ronaldogomes96 <gomes.ronaldo96@gmail.com>
2020-08-14 11:56:48 -0300

    Creation of the tabBar Controller class and assignment to the corresponding storyboard

---
+++ TheUniverse/TheUniverse/Controller/FisrtPage/TabBarViewController.swift
@@ -0,0 +1,21 @@
+//
+//  TabBarViewController.swift
+//  TheUniverse
+//
+//  Created by Ronaldo Gomes on 14/08/20.
+//  Copyright © 2020 Ronaldo Gomes. All rights reserved.
+//
+
+import UIKit
+
+class TabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+        
+    
+
+}
