62afa3ec34f302e06b0262a8f71e9a3e6e82d273
ronaldogomes96 <gomes.ronaldo96@gmail.com>
2020-08-16 02:54:36 -0300

    Json with data from celestial bodies, definition of assets from the first page and creation of the class of celestial bodies

---
+++ TheUniverse/TheUniverse/Model/CelestialBodyModel.swift
@@ -0,0 +1,24 @@
+//
+//  CelestialBodyModel.swift
+//  TheUniverse
+//
+//  Created by Ronaldo Gomes on 16/08/20.
+//  Copyright © 2020 Ronaldo Gomes. All rights reserved.
+//
+
+import Foundation
+
+//Fazer uma enum com os nomes dos corpos celestes ao inves de um switch case
+class CelestialBodyModel {
+    
+    func getListOfNamesAndImagesOfCelestialBody(from celestialBodyType: String) -> ([String], [String])? {
+        
+        return nil
+    }
+    
+    func getCelestialBodyFromJson() -> CelestialBody? {
+        
+        return nil
+    }
+    
+}
