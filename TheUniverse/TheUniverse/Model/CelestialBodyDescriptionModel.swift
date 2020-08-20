ccbd423301a7c6d67efe70dce606aee155b7cd93
ronaldogomes96 <gomes.ronaldo96@gmail.com>
2020-08-20 09:52:29 -0300

    Initial model of CelestialBodyDescription, with default values for creating unit tests

---
+++ TheUniverse/TheUniverse/Model/CelestialBodyDescriptionModel.swift
@@ -0,0 +1,23 @@
+//
+//  CelestialBodyDescriptionModel.swift
+//  TheUniverse
+//
+//  Created by Ronaldo Gomes on 20/08/20.
+//  Copyright © 2020 Ronaldo Gomes. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CelestialBodyDescriptionModel {
+
+    func getCelestialBodyDescription(celestialBody: String) -> String {
+
+        return ""
+    }
+
+    func getCelestialBodyDescriptionFromJson(jsonName: String) -> CelestialBodyDescription? {
+
+        return nil
+    }
+}
