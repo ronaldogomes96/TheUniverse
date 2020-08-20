73ac5ce04f21b1d9b2808ea93d9530888c124307
ronaldogomes96 <gomes.ronaldo96@gmail.com>
2020-08-20 11:00:15 -0300

    Final model of CelestialBodyDescription

--- TheUniverse/TheUniverse/Model/CelestialBodyDescriptionModel.swift
+++ TheUniverse/TheUniverse/Model/CelestialBodyDescriptionModel.swift
@@ -13,11 +13,32 @@ class CelestialBodyDescriptionModel {
 
     func getCelestialBodyDescription(celestialBody: String) -> String {
 
-        return ""
+        let celestialBodyName = CelestialBodyNames(rawValue: celestialBody)?.englishNameOfCelestialBody
+        let celestialBodyDescriptionFromJson = getCelestialBodyDescriptionFromJson(jsonName: celestialBodyName!)
+        let description = celestialBodyDescriptionFromJson?.description
+
+        return description ?? ""
     }
 
     func getCelestialBodyDescriptionFromJson(jsonName: String) -> CelestialBodyDescription? {
 
-        return nil
+        let filePath = Bundle.main.url(forResource: jsonName, withExtension: "json")
+        guard let fileUrlPath = filePath else {
+            return nil
+        }
+
+        let jsonData = try? Data(contentsOf: fileUrlPath)
+        guard let data = jsonData else {
+            return nil
+        }
+
+        let celestialBodyDescription: CelestialBodyDescription
+        do {
+            celestialBodyDescription = try JSONDecoder().decode(CelestialBodyDescription.self, from: data)
+        } catch {
+            return nil
+        }
+
+        return celestialBodyDescription
     }
 }
