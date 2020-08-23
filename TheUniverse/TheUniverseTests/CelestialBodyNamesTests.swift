e70e300314dbbc2ba20e8675e18f864230123694
ronaldogomes96 <gomes.ronaldo96@gmail.com>
2020-08-23 01:07:11 -0300

    Test for CelestialBodyNames enum

---
+++ TheUniverse/TheUniverseTests/CelestialBodyNamesTests.swift
@@ -0,0 +1,44 @@
+//
+//  CelestialBodyNamesTests.swift
+//  TheUniverseTests
+//
+//  Created by Ronaldo Gomes on 23/08/20.
+//  Copyright © 2020 Ronaldo Gomes. All rights reserved.
+//
+
+import XCTest
+@testable import TheUniverse
+
+class CelestialBodyNamesTests: XCTestCase {
+
+    func test_englishNameOfCelestialBody_translateNames_returnsCelestialBodyEnglish() {
+
+        //Given
+        let sut = CelestialBodyNames.self
+
+        //When
+        let listOfCelestialBodyPortuguese = ["Mercúrio", "Vênus", "Terra",
+                                             "Marte", "Júpiter", "Saturno",
+                                             "Urano", "Neturno", "Plutão",
+                                             "Lua", "Fobos", "Ganimedes",
+                                             "Calisto", "Io", "Titã", "Reia",
+                                             "Encélado", "Titânia", "Tritão",
+                                             "Caronte", "Sol"]
+        let celestialBodyEnglish = ["mercury", "venus", "earth",
+                                    "mars", "jupiter", "saturn",
+                                    "uranus", "neptune", "pluto",
+                                    "moon", "phobos", "ganymede",
+                                    "callisto", "io", "titan",
+                                    "rhea", "enceladus", "titania",
+                                    "triton", "charon", "sun"]
+        var listOfCelestialBodyEnglish: [String] = []
+        for index in 0..<listOfCelestialBodyPortuguese.count {
+            listOfCelestialBodyEnglish.append(sut.init(rawValue:
+                listOfCelestialBodyPortuguese[index])!.englishNameOfCelestialBody)
+        }
+
+        //Then
+        XCTAssertEqual(listOfCelestialBodyEnglish, celestialBodyEnglish)
+    }
+
+}
